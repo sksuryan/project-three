e91d8f2542be05e3111ad61e8148aa81e4de9b8d
Dipanwita <susiejojo1@gmail.com>
2020-10-10 20:46:29 +0530

    Create Flask App + Mondo Atlas

---
+++ backend/app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+from flask_pymongo import pymongo
+from .config import DB_URL
+
+MONGO_URL = DB_URL
+
+app = Flask(__name__)
+client = pymongo.MongoClient(MONGO_URL)
+db = client.speech_connect
+
+@app.route("/")
+def hello():
+	return "Hello World!"
+
